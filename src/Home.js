c190b5fd217393ea4366723e9b217731539f6216
chesere <chessm755@gmail.com>
2024-02-17 15:41:41 +0300

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const handleClick = ()=>{
+        console.log("Hello, Ninjas");
+    };
     return ( 
         <div className="home">
-            <p></p>
+            <h1>Homepage</h1>
+            <button onClick={handleClick} >Click me</button>
         </div>
      );
 }
